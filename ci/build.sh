ba337ddb529867691f204c8fe4d23203802f54f2
Marc Brändli <marc.braendli@bbv.ch>
2025-10-08 09:34:39 +0000

    build: build out of source with scripts

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-qmake
-make -j `nproc`
+set -e
 
+# Create build directory
+mkdir -p build
+cd build
+
+# Configure with qmake
+qmake ..
+
+# Build with all available cores
+make -j `nproc`
